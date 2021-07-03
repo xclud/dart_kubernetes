0a2ee1e49a317e69d8eb69c0a9b7c4569b3c1990
Mahdi <mkfard@outlook.com>
2021-07-03 17:51:40 +0430

    WIP client rest functions.

--- lib/kubernetes.dart
+++ lib/kubernetes.dart
@@ -2,4 +2,12 @@ library kubernetes;
 
 export 'src/client.dart';
 export 'src/config.dart';
-export 'src/v1.dart';
+
+export 'api_admissionregistration_v1.dart';
+export 'api_apps_v1.dart';
+export 'api_authentication_v1.dart';
+export 'api_autoscaling_v1.dart';
+export 'api_batch_v1.dart';
+export 'api_core_v1.dart';
+export 'api_policy_v1.dart';
+export 'apimachinery_pkg_apis_meta_v1.dart';
